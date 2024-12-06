782656d025bdcc150f3764f5e25379785ed50f97
christmaskid <chengyt2002@gmail.com>
2024-12-06 15:19:40 +0800

    Debug

--- Mix-of-Show/mixofshow/train.py
+++ Mix-of-Show/mixofshow/train.py
@@ -128,6 +128,8 @@ def train_pipeline(root_path):
 
     # create model
     model = build_model(opt)
+    return # debug
+
     if resume_state:  # resume training
         model.resume_training(resume_state)  # handle optimizers and schedulers
         logger.info(f"Resuming training from epoch: {resume_state['epoch']}, " f"iter: {resume_state['iter']}.")
