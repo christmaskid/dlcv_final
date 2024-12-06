75a0e9ac431f084ccff7689bf86dd3a55f2c7cb7
root <root@LAPTOP-LG7CJA98>
2024-12-07 00:31:40 +0800

    Upload merge script for inference

--- Mix-of-Show/scripts/mixofshow_scripts/paper_result_scripts/mix_of_show_cat2_dog6.sh
+++ Mix-of-Show/scripts/mixofshow_scripts/paper_result_scripts/mix_of_show_cat2_dog6.sh
@@ -4,9 +4,9 @@ expdir="cat2+dog6"
 context_prompt="A <cat2> on the right and a <dog6> on the left."
 python inference/mix_of_show_sample.py \
   --pretrained_model="experiments/pretrained_models/stable-diffusion-v1-4" \
-  --combined_model="${combined_model_root}/${expdir}/combined_model_base.pth" \
+  --combined_model="${combined_model_root}/${expdir}/combined_model_.pth" \
   --save_dir="results/multi-concept/${expdir}" \
   --pipeline_type="sd_pplus" \
   --prompt="${context_prompt}" \
-  --suffix="_" \
+  --suffix="_241207" \
   --seed=641
