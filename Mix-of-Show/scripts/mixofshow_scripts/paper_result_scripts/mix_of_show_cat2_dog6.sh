4d03a9f61c36b347efe10e95f93926b8e04f3dfe
root <root@LAPTOP-LG7CJA98>
2024-12-07 00:27:34 +0800

    Upload merge script for inference

---
+++ Mix-of-Show/scripts/mixofshow_scripts/paper_result_scripts/mix_of_show_cat2_dog6.sh
@@ -0,0 +1,12 @@
+combined_model_root="experiments/composed_edlora/stable-diffusion-v1-4/"
+expdir="cat2+dog6"
+
+context_prompt="A <cat2> on the right and a <dog6> on the left."
+python inference/mix_of_show_sample.py \
+  --pretrained_model="experiments/pretrained_models/stable-diffusion-v1-4" \
+  --combined_model="${combined_model_root}/${expdir}/combined_model_base.pth" \
+  --save_dir="results/multi-concept/${expdir}" \
+  --pipeline_type="sd_pplus" \
+  --prompt="${context_prompt}" \
+  --suffix="_" \
+  --seed=641
