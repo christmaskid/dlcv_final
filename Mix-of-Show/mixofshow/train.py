f7d18e4e94ae651873436d32756c9c916a22be71
christmaskid <chengyt2002@gmail.com>
2024-12-06 15:44:47 +0800

    Debug

--- Mix-of-Show/mixofshow/train.py
+++ Mix-of-Show/mixofshow/train.py
@@ -128,7 +128,7 @@ def train_pipeline(root_path):
 
     # create model
     model = build_model(opt)
-    print(model.get_bare_model(model.net_g).delta_state_dict().keys())
+    print(model.get_bare_model(model.net_g).delta_state_dict())
     return # debug
 
     if resume_state:  # resume training
