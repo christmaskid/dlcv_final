6bfc8bf385b17065080d68ee81f6ef3b8ff46ad8
christmaskid <chengyt2002@gmail.com>
2024-12-06 11:23:27 +0800

    hyperparameters

--- Mix-of-Show/dataset_convert.py
+++ Mix-of-Show/dataset_convert.py
@@ -16,6 +16,11 @@ def convert(args):
 	check_path(args.out_captions_path)
 	check_path(args.out_jsons_path)
 
+	f = open(args.prompts_path, "w")
+	for prompt in prompts:
+		f.write(prompt+"\n")
+	f.close()
+
 	for token_name in os.listdir(args.input_images_path):
 		print(token_name)
 		caption_dir = os.path.join(args.out_captions_path, token_name)
@@ -42,7 +47,21 @@ def convert(args):
 		yaml_content = yaml_content.replace("<name>", token_name)
 		yaml_content = yaml_content.replace("<new_concept_token>", "<"+token_name+">")
 		yaml_content = yaml_content.replace("<concept_list>", out_json_path)
-		yaml_content = yaml_content.replace("<prompts>", args.prompts_path)
+		yaml_content = yaml_content.replace("<prompts_path>", args.prompts_path)
+
+		yaml_content = yaml_content.replace("<embedding_enable_tuning>", args.embedding_enable_tuning)
+		yaml_content = yaml_content.replace("<text_encoder_enable_tuning>", args.text_encoder_enable_tuning)
+		yaml_content = yaml_content.replace("<unet_enable_tuning>", args.unet_enable_tuning)
+		yaml_content = yaml_content.replace("<embedding_lr>", str(args.embedding_lr))
+		yaml_content = yaml_content.replace("<text_encooder_lr>", str(args.text_encooder_lr))
+		yaml_content = yaml_content.replace("<unet_lr>", str(args.unet_lr))
+		yaml_content = yaml_content.replace("<n_iterations>", str(args.n_iterations))
+		yaml_content = yaml_content.replace("<lora_rank>", str(args.lora_rank))
+		yaml_content = yaml_content.replace("<lora_alpha>", str(args.lora_alpha))
+		yaml_content = yaml_content.replace("<latent_size>", str(args.latent_size))
+
+		yaml_content = yaml_content.replace("<num_inference_steps>", str(args.num_inference_steps))
+		yaml_content = yaml_content.replace("<guidance_scale>", str(args.guidance_scale))
 		
 		token_yaml_file.write(yaml_content)
 		token_yaml_file.close()
@@ -68,13 +87,28 @@ def convert(args):
 
 if __name__=="__main__":
 	parser = argparse.ArgumentParser()
-	parser.add_argument("--input_images_path")
-	parser.add_argument("--input_json_path")
-	parser.add_argument("--out_captions_path")
-	parser.add_argument("--out_jsons_path")
-	parser.add_argument("--template_yaml")
-	parser.add_argument("--prompts_path")
-	parser.add_argument("--merge_json_path_prefix")
+	parser.add_argument("--input_images_path", required=True)
+	parser.add_argument("--input_json_path", required=True)
+	parser.add_argument("--out_captions_path", required=True)
+	parser.add_argument("--out_jsons_path", required=True)
+	parser.add_argument("--template_yaml", required=True)
+	parser.add_argument("--prompts_path", required=True)
+	parser.add_argument("--merge_json_path_prefix", required=True)
+
+	parser.add_argument("--embedding_enable_tuning", default="true")
+	parser.add_argument("--text_encoder_enable_tuning", default="true")
+	parser.add_argument("--unet_enable_tuning", default="true")
+	parser.add_argument("--embedding_lr", default=1e-3)
+	parser.add_argument("--text_encoder_lr", default=1e-5)
+	parser.add_argument("--unet_lr", default=1e-4)
+	parser.add_argument("--n_iterations", default=1000)
+	parser.add_argument("--lora_rank", default=4)
+	parser.add_argument("--lora_alpha", default=1)
+	parser.add_argument("--latent_size", default=[4,64,64])
+
+	parser.add_argument("--num_inference_steps", default=50)
+	parser.add_argument("--guidance_scale", default=7.5)
+
 	args = parser.parse_args()
 
 	convert(args)
