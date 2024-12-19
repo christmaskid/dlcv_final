905ee05106b92ff7c159ce2b09f27fc5a109111a
root <root@LAPTOP-LG7CJA98>
2024-12-19 20:14:51 +0800

    config for sd7: no overlap on vertical direction & full height mask

--- Concept-Conductor/generate_random_points.py
+++ Concept-Conductor/generate_random_points.py
@@ -33,11 +33,19 @@ if len(sys.argv) > 2 and isinstance(eval(sys.argv[2]), list):
 
 else:
 	while len(bboxes) < n_concept:
-		center = [random.randint(img_height//6, img_height*5//6-1), 
-				  random.randint(img_width//6, img_width*5//6-1)]
-		h = img_height #random.randint(img_height//5, img_height//2-1)
-		w = random.randint(img_width//5, img_width//4-1)
-		bbox = [center[0]-h//2, center[1]-w//2, center[0]+h//2, center[1]+w//2]
+		# center = [random.randint(img_height//6, img_height*5//6-1), 
+		# 		  random.randint(img_width//6, img_width*5//6-1)]
+		# h = random.randint(img_height//5, img_height//2-1)
+		# w = random.randint(img_width//5, img_width//4-1)
+
+		center = [img_height//2, random.randint(img_width//n_concept*len(bboxes-1), img_width//n_concept*len(bboxes))]
+		h = img_height
+		w = random.randint(img_width//5, img_width//n_concept)
+
+		bbox = [
+			max(0, center[0]-h//2), max(0, center[1]-w//2), 
+			min(img_height-1, center[0]+h//2), min(img_width-1, center[1]+w//2)
+		]
 		print(bbox)
 
 		flag = False
