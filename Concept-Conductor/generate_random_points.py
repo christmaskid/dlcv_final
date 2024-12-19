e2a4a34c4c3dbf65f7b6450b6e565cfba061da8c
root <root@LAPTOP-LG7CJA98>
2024-12-19 20:06:09 +0800

    config for sd7: no overlap on vertical direction & full height mask

--- Concept-Conductor/generate_random_points.py
+++ Concept-Conductor/generate_random_points.py
@@ -36,7 +36,7 @@ else:
 		center = [random.randint(img_height//6, img_height*5//6-1), 
 				  random.randint(img_width//6, img_width*5//6-1)]
 		h = img_height #random.randint(img_height//5, img_height//2-1)
-		w = random.randint(img_width//5, img_width//3-1)
+		w = random.randint(img_width//5, img_width//4-1)
 		bbox = [center[0]-h//2, center[1]-w//2, center[0]+h//2, center[1]+w//2]
 		print(bbox)
 
