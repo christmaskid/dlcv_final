72bc84a6044b9d55550793ed9580bfac8b0d713a
root <root@LAPTOP-LG7CJA98>
2024-12-19 21:27:13 +0800

    config for sd8: make fixed bigger masks; turn off layout alignment

--- Concept-Conductor/generate_random_points.py
+++ Concept-Conductor/generate_random_points.py
@@ -17,10 +17,10 @@ def collide(a, b):
 	# x: vertical, y: horizontal
 	ax1, ay1, ax2, ay2 = a
 	bx1, by1, bx2, by2 = b
-	return (between(ax1, bx1, bx2) or between(bx1, ax1, ax2)) # and (between(ay1, by1, by2) or between(by1, ay1, ay2))
+	return (between(ax1, bx1, bx2) or between(bx1, ax1, ax2)) and (between(ay1, by1, by2) or between(by1, ay1, ay2))
 
 def create_and_save_mask(bboxes, save_fn):
-	mask = Image.new("L", (img_width, img_height), 0)
+	mask = Image.new("L", (img_height, img_width), 0)
 	draw = ImageDraw.Draw(mask)
 	for bbox in bboxes:
 		draw.rectangle(bbox, fill=255)
@@ -31,30 +31,36 @@ def create_and_save_mask(bboxes, save_fn):
 if len(sys.argv) > 2 and isinstance(eval(sys.argv[2]), list):
 	bboxes = eval(sys.argv[2])
 
-else:
-	while len(bboxes) < n_concept:
-		# center = [random.randint(img_height//6, img_height*5//6-1), 
-		# 		  random.randint(img_width//6, img_width*5//6-1)]
-		# h = random.randint(img_height//5, img_height//2-1)
-		# w = random.randint(img_width//5, img_width//4-1)
-
-		center = [int(img_width//n_concept*(len(bboxes)+0.5)), img_height//2]
-		h = img_height
-		w = random.randint(img_width//5, img_width//(n_concept+1))
-
-		bbox = [
+margin_h = img_height//3
+margin_w = img_width//(n_concept+1)
+while len(bboxes) < n_concept:
+	center = [
+			  # random.randint(margin_w, img_height-1-margin_w), 
+			  # random.randint(
+			  # 	max(margin_w, img_width//n_concept*len(bboxes)), 
+			  # 	min(img_width-margin_w, img_width//n_concept*(len(bboxes)+1))
+			  # ),
+			  int(img_width//n_concept*(len(bboxes)+0.5)),
+			  random.randint(img_height//2, img_height-1-margin_h),
+			  ]
+	h = random.randint(margin_h, img_height//2-1)
+	w = img_width//(n_concept+1) #random.randint(img_width//(n_concept+1), img_width//n_concept)
+	bbox = [
 			max(0, center[0]-w//2), max(0, center[1]-h//2), 
 			min(img_width-1, center[0]+w//2), min(img_height-1, center[1]+h//2), 
 		]
-		print(center, h, w, bbox, bboxes)
+	print(
+			  	max(margin_w, img_width//n_concept*len(bboxes)), 
+			  	min(img_width-margin_w, img_width//n_concept*(len(bboxes)+1)))
+	print(center, w, h,  bbox, bboxes)
 
-		flag = False
-		for other in bboxes:
-			flag = collide(bbox, other)
-			if flag: break
-		if not flag: bboxes.append(bbox)
+	flag = False
+	for other in bboxes:
+		flag = collide(bbox, other)
+		if flag: break
+	if not flag: bboxes.append(bbox)
 
-print(bboxes)
+random.shuffle(bboxes)
 for i, bbox in enumerate(bboxes):
 	mask = create_and_save_mask([bbox], "examples/{}_{}.png".format(task_name, i+1))
 create_and_save_mask(bboxes, "examples/{}_mask.png".format(task_name))
