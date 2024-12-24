75202b8778e4afbe0bed7cae9abaf4b1bcfb1945
root <root@LAPTOP-LG7CJA98>
2024-12-25 02:27:54 +0800

    Upload checkpoints and README format.

---
+++ DLCV-Fall-2024-Final-2-dontlookcanvis/Concept-Conductor/data_format.py
@@ -0,0 +1,154 @@
+import os
+import json
+import random
+import argparse
+
+prompts = [
+	"a photo of <TOK>"
+]
+
+semantics = {
+	"cat2": "cat",
+	"pet_cat1": "cat",
+	"dog": "dog",
+	"dog6": "dog",
+	"flower_1": "flower",
+	"wearable_glasses": "glasses",
+	"watercolor": "watercolor",
+	"vase": "vase"
+}
+
+all_region_prompts = {
+	"0": [
+		("A <cat2_1> <cat2_2> on the right.", '[84,263,446,512]'), 
+		("A <dog6_1> <dog6_2> on the left.", '[113,15,410,245]')
+		],
+	"1": [
+		("A <flower_1_1> <flower_1_2>.", "[77,152,383,287]"), 
+		("A <vase_1> <vase_2>", "[212,152,482,331]")
+		],
+	"2": [
+		("A <dog_1> <dog_2> near a forest.", "[213,6,493,276]"), 
+		("A <pet_cat1_1> <pet_cat1_2> near a forest.", "[0,161,257,362]"),
+		("A <dog6_1> <dog6_2> near a forest.","[263,298,512,512]")
+		],
+	"3": [
+		("A <cat2_1> <cat2_2> in a <watercolor_1> <watercolor_2> style.","[107,96,512,406]"), 
+		("A <wearable_glasses_1> <wearable_glasses_2> in a <watercolor_1> <watercolor_2> style.","[169,85,230,391]")
+		]
+}
+
+sketches = {
+	"0": "cat_dog.png",
+	"1": "flower_vase.png",
+	"2": "dog_cat_dog.png",
+	"3": "cat_glasses.png"
+}
+
+new_concepts_tokens = {}
+
+def check_path(_path):
+	if not os.path.exists(_path):
+		os.mkdir(_path)
+
+def convert(args):
+	print(args)
+	check_path(args.out_captions_path)
+	check_path(args.out_jsons_path)
+
+	f = open(args.prompts_path, "w")
+	for prompt in prompts:
+		f.write(prompt+"\n")
+	f.close()
+
+	for token_name in os.listdir(args.input_images_path):
+		print(token_name)
+		caption_dir = os.path.join(args.out_captions_path, token_name)
+		check_path(caption_dir)
+
+		image_dir = os.path.join(args.input_images_path, token_name)
+		for img_fn in os.listdir(image_dir):
+			txt_file = open(os.path.join(caption_dir, img_fn.split(".")[0]+".txt"), "w")
+			txt_file.write(random.choice(prompts))
+			txt_file.close()
+
+		output_json_file = [{
+			"instance_prompt": "<TOK>",
+			"instance_data_dir": image_dir,
+			"caption_dir": caption_dir
+		}]
+		out_json_path = os.path.join(args.out_jsons_path, token_name+".json")
+		json.dump(output_json_file, open(out_json_path, "w"))
+
+		for prefix, template_fn in [("", args.template_yaml)]:#, ("test_",args.test_template_yaml)]:
+			template_yaml_file = open(template_fn, "r")
+			token_yaml_file = open(prefix+token_name+".yml", "w")
+
+			yaml_content = ''.join(template_yaml_file.readlines())
+
+			new_concepts = ["<{}_1>".format(token_name), "<{}_2>".format(token_name)]
+			new_concepts_tokens[token_name] = new_concepts
+
+			yaml_content = yaml_content.replace("<name>", token_name)
+			yaml_content = yaml_content.replace("<replace_mapping>", " ".join(new_concepts))
+			yaml_content = yaml_content.replace("<new_concept_token>", "+".join(new_concepts))
+			yaml_content = yaml_content.replace("<concept_list>", out_json_path)
+			yaml_content = yaml_content.replace("<prompts_path>", args.prompts_path)
+			yaml_content = yaml_content.replace("<pretrained_path>", args.pretrained_path)
+
+			yaml_content = yaml_content.replace("<semantic>", semantics[token_name])
+
+			yaml_content = yaml_content.replace("<embedding_enable_tuning>", args.embedding_enable_tuning)
+			yaml_content = yaml_content.replace("<text_encoder_enable_tuning>", args.text_encoder_enable_tuning)
+			yaml_content = yaml_content.replace("<unet_enable_tuning>", args.unet_enable_tuning)
+			yaml_content = yaml_content.replace("<embedding_lr>", str(args.embedding_lr))
+			yaml_content = yaml_content.replace("<text_encoder_lr>", str(args.text_encoder_lr))
+			yaml_content = yaml_content.replace("<unet_lr>", str(args.unet_lr))
+			yaml_content = yaml_content.replace("<n_iterations>", str(args.n_iterations))
+			yaml_content = yaml_content.replace("<lora_rank>", str(args.lora_rank))
+			yaml_content = yaml_content.replace("<lora_alpha>", str(args.lora_alpha))
+			yaml_content = yaml_content.replace("<latent_size>", str(args.latent_size))
+
+			yaml_content = yaml_content.replace("<num_inference_steps>", str(args.num_inference_steps))
+			yaml_content = yaml_content.replace("<guidance_scale>", str(args.guidance_scale))
+
+			yaml_content = yaml_content.replace("<save_checkpoint_freq>", str(args.save_checkpoint_freq))
+			
+			token_yaml_file.write(yaml_content)
+			token_yaml_file.close()
+			template_yaml_file.close()
+
+
+if __name__=="__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--input_images_path", required=True)
+	parser.add_argument("--input_json_path", required=True)
+	parser.add_argument("--out_captions_path", required=True)
+	parser.add_argument("--out_jsons_path", required=True)
+	parser.add_argument("--template_yaml", required=True)
+	parser.add_argument("--test_template_yaml", required=True)
+	parser.add_argument("--prompts_path", required=True)
+	parser.add_argument("--merge_json_path_prefix", required=True)
+	parser.add_argument("--pretrained_path", required=True)
+
+	parser.add_argument("--neg_prompt", default="")
+
+	parser.add_argument("--embedding_enable_tuning", default="true")
+	parser.add_argument("--text_encoder_enable_tuning", default="true")
+	parser.add_argument("--unet_enable_tuning", default="true")
+	parser.add_argument("--embedding_lr", default=1e-3)
+	parser.add_argument("--text_encoder_lr", default=1e-5)
+	parser.add_argument("--unet_lr", default=1e-4)
+	parser.add_argument("--n_iterations", default=1000)
+	parser.add_argument("--lora_rank", default=4)
+	parser.add_argument("--lora_alpha", default=1)
+	parser.add_argument("--latent_size", default=[4,64,64])
+
+	parser.add_argument("--num_inference_steps", default=50)
+	parser.add_argument("--guidance_scale", default=7.5)
+
+	parser.add_argument("--save_checkpoint_freq", default=10000)
+
+	args = parser.parse_args()
+
+	convert(args)
--- DLCV-Fall-2024-Final-2-dontlookcanvis/README.md
+++ DLCV-Fall-2024-Final-2-dontlookcanvis/README.md
@@ -1,32 +1,77 @@
 # DLCV Final Project ( Multiple Concept Personalization )
 
-# How to run your code?
-* TODO: Please provide the scripts for TAs to reproduce your results, including training and inference. For example, 
+## Checkpoints download
+Please download all the checkpoints from: https://drive.google.com/drive/folders/1u2un5sqWEY7yj-U-1wgyN41BICqQ3wWm?usp=sharing
 
+## Usage
+
+    git clone https://github.com/DLCV-Fall-2024/DLCV-Fall-2024-Final-2-dontlookcanvis.git
+    cd Concept-Conductor
+    pip install -r requirements.txt
+
+### General Training and Inference
 ```shell script=
 bash train.sh <path/to/config>
 bash inference.sh <path/to/config>
 ```
-Eg. <path/to/config> = configs/train/stable-diffusion-v1-5/cat2.yml
+```<path/to/config>``` example: ```configs/train/stable-diffusion-v1-5/cat2.yml```
 
-# Usage
-To start working on this final project, you should clone this repository into your local machine by the following command:
+### Peer review
+0. python scripts/... --xxx
 
-    git clone https://github.com/DLCV-Fall-2024/DLCV-Fall-2024-Final-2-dontlookcanvis.git
-  
-Note that you should replace `<team_name>` with your own team name.
+1.
 
-For more details, please click [this link](https://docs.google.com/presentation/d/1eeXx_dL0OgkDn9_lhXnimTHrE6OYvAiiVOBwo2CTVOQ/edit?usp=sharing) to view the slides of Final Project - Multiple Concept Personalization. **The introduction video for final project can be accessed in the slides.**
+2.
+
+3.
+
+### CodaLab uploads
+0.
+
+1.
+
+2.
+
+3.
 
 
-### Mix of Show
-Composed ED-LoRAs checkpoints: https://drive.google.com/drive/folders/1hsmbVbIrMK4EtPFHwlGLz1jd17ajbukv?usp=sharing
+### Poster results
 
-For reproduction of mix-of-show results, please download this repository with ```--recursive```, set up environment in ```Mix-of-Show```, download the composed ED-LoRAs from the above link and add soft links as
+#### Attention Clustering Post-processing
+
+
+#### Mix of Show
+For reproduction of mix-of-show results, please clone this repository with ```--recursive``` to download the Mix-of-Show submodule.
+Please follow the environment setup in Mix of Show repository.
 ```
-ln -s <path/of/composed_edlora> experiments/
+    cd Mix-of-Show
+    python setup.py install
+    
+    # Clone diffusers==0.14.0 with T2I-Adapter support
+    git clone https://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p@github.com/guyuchao/diffusers-t2i-adapter.git
+
+    # switch to T2IAdapter-for-mixofshow
+    %cd diffusers-t2i-adapter
+    git switch T2IAdapter-for-mixofshow
+
+    # install from source
+    pip install .
 ```
-Then do
+... etc.
+
+To download checkpoints, execute:
+```
+    cd experiments/pretrained_models
+    gdown 16P7v_WQ46csK_KfXhmkt1iO9ulpkUjq8
+    unzip composed_edlora.zip
+    rm composed_edlora.zip
+```
+Please also download the stable-diffusion v1.4 model, and create soft link:
+```
+    ln -s <path/to/sd-v1-4> experiments/pretrained_models/stable-diffusion-v1-4
+```
+
+For inference, here we take the prompt-0 for example:
 ```
 combined_model_root="experiments/composed_edlora/stable-diffusion-v1-4/"
 expdir="cat2+dog6"
@@ -39,16 +84,6 @@ python Mix-of-Show/inference/mix_of_show_sample.py \
   --pipeline_type="sd_pplus" \
   --prompt="${context_prompt}" \
   --suffix="" \
-  --n_samples=20
+  --n_samples=<n_sample_you_want>
 
 ```
-
-# Submission Rules
-### Deadline
-113/12/26 (Thur.) 23:59 (GMT+8)
-    
-# Q&A
-If you have any problems related to Final Project, you may
-- Use TA hours
-- Contact TAs by e-mail ([ntudlcv@gmail.com](mailto:ntudlcv@gmail.com))
-- Post your question under `[Final challenge 2] Discussion` section in NTU Cool Discussion
